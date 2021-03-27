febd90044f1d091654bebccdc44f400cb75d527c
bhat shakran <bhatshakran@gmail.com>
2021-03-27 11:50:23 +0530

    import statement in heroBanner modified

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react"
+import * as React from "react"
+import { useEffect, useState } from "react"
 import "./styles/HeroBanner.scss"
 import { Typography } from "antd"
 import TextTransition, { presets } from "react-text-transition"
